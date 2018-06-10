2e7cdc47eaaaac343f9a91f992f337012c771b1e
doublejbs <doublejbs@naver.com>
2018-06-10 21:57:09 +0900

    제출

--- assignment4/receiver.py
+++ assignment4/receiver.py
@@ -3,28 +3,31 @@ import time
 import datetime
 from queue import Queue
 
+#bottlenec rate와 queue size를 입력받습니다.
 botRate = int(input("bottle neck link rate: "))
 qSize = int(input("queue size: "))
-print(socket.gethostbyname(socket.gethostname()))
+
+#socket을 생성합니다.
 receiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 receiver.bind((socket.gethostbyname(socket.gethostname()), 10080))
 
-agQueue = []
 
-once = 1
-neOnce = 1
 occList = {}
-inPck = 0
-forPck = 0
+inPck = 0 #incoming packet을 count합니다.
+forPck = 0 #forwarding packet을 count합니다.
+
 
 def wait():
     t = threading.Thread(target=wait)
     t.start()
 
+#network emulator모듈에서 bottleneck실행 후 Ack generator 모듈로 forwarding하는 함수 입니다.
 def ne2ag(sock, addr, botRate):
-    global run, forPck, neOnce, go
+    global run, forPck, go
+    #sender에 해당하는 queue를 받아옵니다.
     neQueue = qList.get(addr[1])
     _go = go.get(addr[1])
+    #forwarding 후 sender에게 ack을 보냅니다.
     if _go:
         neQueue.get()
         forPck += 1
@@ -34,17 +37,20 @@ def ne2ag(sock, addr, botRate):
     if run.get(addr[1]):
         timer.start()
 
+#network emulator에서 packet을 queue에 추가하는 함수입니다.
 def ntwrkEmul(qSize, data, addr):
-    global runOnce
+
     neQueue = qList.get(addr[1])
+    #queue가 다 찼으면 drop 시킵니다.
     if neQueue.qsize() < qSize:
         neQueue.put(data.decode())
     else:
         pass
-    #bottleneck link rate동안 ne에서 ag로!
 
+
+#2초마다 출력하는 메시지 함수입니다.
 def twoSecMsg():
-    global once, run, qSize
+    global run, qSize
     global inPck, forPck, occList
 
     try:
@@ -54,7 +60,7 @@ def twoSecMsg():
         inPck = 0
         forPck = 0
     except Exception as e:
-        print('1', e)
+        print(e)
 
     timer = threading.Timer(2.1, twoSecMsg)
     if 1:
@@ -67,6 +73,7 @@ go = {}
 run = {}
 addrList = []
 
+#각 sender마다 queue에서의 occupancy의 값을 0.1초마다 추가합니다.
 def avgOccupancy(addr):
     global occList, qList, run
     occ = qList.get(addr[1]).qsize()
@@ -75,6 +82,7 @@ def avgOccupancy(addr):
     if run.get(addr[1]):
         t.start()
 
+#queue의 평균 occupancy값을 구하는 함수입니다.
 def getAvgOcc():
     global occList, addrList
     i = 0
@@ -91,13 +99,15 @@ def getAvgOcc():
     return sum
 
 
-
+#packet을 받을 때의 함수입니다.
 def rcvMsg(sock):
     global inPck, qSize, index, qList, go, run, addrList
     while True:
         try:
             data, addr = sock.recvfrom(1024)
 
+            #sender가 처음 접속시 enter 메시지를 받고, sender를 리스트에 추가합니다.
+            #sender마다 queue를 생성합니다.
             if data.decode() == 'enter':
                 addrList.append(addr[1])
                 run[addr[1]] = 1
@@ -108,19 +118,17 @@ def rcvMsg(sock):
                 ne2ag(sock, addr, botRate)
                 avgOccupancy(addr)
                 go[addr[1]] = 1
-            elif data.decode() == 'exit':
-                run[addr[1]] = 0
 
+            #처음 접속이후 packet을 받을 때마다 incoming pakcet을 증가시키고 ntwrkEmul함수를 실행합니다.
             elif data:
                 inPck += 1
                 ntwrkEmul(qSize, data, addr)
-
         except:
             continue
 
 
 addr = []
-runOnce = 1
+
 wait()
 twoSecMsg()
 t = threading.Thread(target=rcvMsg, args=(receiver,))
--- assignment4/sender3.py
+++ assignment4/sender3.py
@@ -1,25 +1,22 @@
 import socket, threading
 import time
 
-#receiver의 ip와 init send rate를 입력받습니다.
+
 ip = input("enter receiver ip: ")
 initSendRate = int(input("enter initial sending rate: "))
 
-#socket을 생성합니다.
+
 sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sender.bind((socket.gethostbyname(socket.gethostname()), 0))
 ackCount = 0
 once = 1
 
 j=0
-
-#1000byte 짜리 text를 생성합니다.
 text = '11111'
 while j<199:
     text = '11111' + text
     j += 1
 
-#receiver로 부터 ack을 받을 때마다 ackCount를 증가시킵니다.
 def rcvMsg(sock):
     global ackCount
     while True:
@@ -27,14 +24,13 @@ def rcvMsg(sock):
         if data:
             ackCount += 1
 
-#2초마다 출력되는 메시지 함수입니다.
 def twoSecMsg():
     global once
     if once:
         time.sleep(2)
         once = 0
     global pckCount, ackCount
-
+    #print('temp: ', datetime.datetime.now())
     print("sending rate: ", pckCount/2, "/2sec")
     print("goodput: ", ackCount/2, "/2sec")
     print("goodput ratio: ", ackCount/pckCount)
@@ -45,20 +41,21 @@ def twoSecMsg():
         timer.start()
 
 
-#receiver에게 packet을 전송하는 함수입니다.
+
 def sendMsg(sock):
     global allPck, pckCount, text
     sender.sendto(text.encode('utf-8'), (ip, 10080))
-    #packet을 전송할 때마다 pckCount를 증가시킵니다.
+    #print('pkt', pckCount, datetime.datetime.now())
+    allPck += 1
     pckCount += 1
     timer = threading.Timer(1/initSendRate, sendMsg, args=[sock])
     if 1:
         timer.start()
 
 
-
-
-
+run = 0
+allPck = 0
+initTime = time.time()
 
 pckCount = 0
 
@@ -66,7 +63,6 @@ t = threading.Thread(target=rcvMsg, args=(sender,))
 t.daemon = True
 t.start()
 
-#처음 접속시 enter 메시지를 보냅니다.
 sender.sendto('enter'.encode('utf-8'), (ip, 10080))
 
 sendMsg(sender)
